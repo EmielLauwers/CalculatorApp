80524fee48e4983067e5d01550f04467cca8330a
EmielLauwers <lauwers.emiel@gmail.com>
2025-02-20 11:46:11 +0100

    alles is nu aangepast

--- calculator/ContentView.swift
+++ calculator/ContentView.swift
@@ -1,65 +1,90 @@
-//
-//  ContentView.swift
-//  calculator
-//
-//  Created by Emiel lauwers on 20/02/2025.
-//
-
 import SwiftUI
 
 enum CalcButton: String {
-    case een
-    case twee
-    case drie
-    case vier
-    case vijf
-    case zes
-    case zeven
-    case acht
-    case negen
-    case nul
-    case add
-    case verminder
-    case deel
-    case maal
-    case gelijk
-    case clear
-    case decimal
-    case procent
-    case negatief
+    case een = "1"
+    case twee = "2"
+    case drie = "3"
+    case vier = "4"
+    case vijf = "5"
+    case zes = "6"
+    case zeven = "7"
+    case acht = "8"
+    case negen = "9"
+    case nul = "0"
+    case add = "+"
+    case verminder = "-"
+    case deel = "÷"
+    case maal = "×"
+    case gelijk = "="
+    case clear = "AC"
+    case decimal = ","
+    case procent = "%"
+    case negatief = "+/−"
+    case calculator = "calculator"
+    
+    var buttonColor: Color {
+        switch self {
+        case .clear, .negatief, .procent:
+            return Color.gray
+        case .add, .verminder, .deel, .maal, .gelijk:
+            return Color.orange
+        default:
+            return Color(.darkGray)
+        }
+    }
 }
 
 struct ContentView: View {
     
-    let butttons: [[CalcButton]] = [
-        [.zeven, .acht, .negen]
+    let buttons: [[CalcButton]] = [
+        [.clear, .negatief, .procent, .deel],
+        [.zeven, .acht, .negen, .maal],
+        [.vier, .vijf, .zes, .verminder],
+        [.een, .twee, .drie, .add],
+        [.calculator, .nul, .decimal, .gelijk]
     ]
     
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
             
-            VStack{
-                HStack{
+            VStack(spacing: 10) {
+                Spacer()
+                HStack {
                     Spacer()
                     Text("0")
                         .bold()
-                        .font(.system(size: 60))
+                        .font(.system(size: 80))
                         .foregroundColor(.white)
-                    
+                        .padding()
                 }
-                .padding()
                 
-                ForEach(butttons, id: \.self) { rij in
-                    ForEach(rij, id: \.self)
-                    
+                ForEach(buttons, id: \.[0].self) { row in
+                    HStack(spacing: 10) {
+                        ForEach(row, id: \.self) { item in
+                            Button(action: {}, label: {
+                                if item == .calculator {
+                                    Image(systemName: "apps.iphone")
+                                        .font(.system(size: 32))
+                                        .frame(width: 80, height: 80)
+                                        .background(Color.gray)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(40)
+                                } else {
+                                    Text(item.rawValue)
+                                        .font(.system(size: 32))
+                                        .frame(width: item == .nul ? 80 : 80, height: 80)
+                                        .background(item.buttonColor)
+                                        .foregroundColor(item == .clear || item == .negatief || item == .procent ? .black : .white)
+                                        .cornerRadius(40)
+                                }
+                            })
+                        }
+                    }
                 }
             }
-            
-            
-            
+            .padding(/*@START_MENU_TOKEN@*/EdgeInsets()/*@END_MENU_TOKEN@*/)
         }
-        
     }
 }
 
