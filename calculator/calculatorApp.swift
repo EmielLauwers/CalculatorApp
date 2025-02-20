9a69d844038aab40bfeedb2758baedea641cf026
EmielLauwers <lauwers.emiel@gmail.com>
2025-02-20 10:49:41 +0100

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Emiel lauwers on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
