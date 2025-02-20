f13e1eab4eb8870d38038bc7547f266d621e29a2
EmielLauwers <lauwers.emiel@gmail.com>
2025-02-20 11:12:45 +0100

    Voeg ZStack toe

--- calculator/ContentView.swift
+++ calculator/ContentView.swift
@@ -7,15 +7,59 @@
 
 import SwiftUI
 
+enum CalcButton: String {
+    case een
+    case twee
+    case drie
+    case vier
+    case vijf
+    case zes
+    case zeven
+    case acht
+    case negen
+    case nul
+    case add
+    case verminder
+    case deel
+    case maal
+    case gelijk
+    case clear
+    case decimal
+    case procent
+    case negatief
+}
+
 struct ContentView: View {
+    
+    let butttons: [[CalcButton]] = [
+        [.zeven, .acht, .negen]
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.ignoresSafeArea()
+            
+            VStack{
+                HStack{
+                    Spacer()
+                    Text("0")
+                        .bold()
+                        .font(.system(size: 60))
+                        .foregroundColor(.white)
+                    
+                }
+                .padding()
+                
+                ForEach(butttons, id: \.self) { rij in
+                    ForEach(rij, id: \.self)
+                    
+                }
+            }
+            
+            
+            
         }
-        .padding()
+        
     }
 }
 
